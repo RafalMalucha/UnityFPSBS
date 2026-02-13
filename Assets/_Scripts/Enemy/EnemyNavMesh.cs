5b072a690faa04a006a804aef69034eb01d52217
Rafal <rafal.z.malucha@gmail.com>
2026-02-13 15:46:04 +0100

    add random destination point selection

--- Assets/_Scripts/Enemy/EnemyNavMesh.cs
+++ Assets/_Scripts/Enemy/EnemyNavMesh.cs
@@ -1,19 +1,47 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyNavMesh : MonoBehaviour
 {
 
-    [SerializeField] private Transform _movePositionTransform;
+    [SerializeField] private GameObject _currentDestination;
+    private Transform _movePositionTransform;
     private NavMeshAgent _navMeshAgent;
 
     private void Awake() 
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+        StartCoroutine(GetRandomDestination());
     }
 
     private void Update()
     {
         _navMeshAgent.destination = _movePositionTransform.position;
+    }
+
+    IEnumerator GetRandomDestination()
+    {
+        while (true)
+        {
+            Debug.Log("Selecting new destination");
+
+            Vector3 newPosition = new Vector3(this.transform.position.x + Random.Range(-5.0f, 5.0f), 0, this.transform.position.z + Random.Range(-5.0f, 5.0f));
+
+            if (!GameObject.Find("NavMeshDestinationPoint(Clone)"))
+            {
+                Instantiate(_currentDestination, newPosition, Quaternion.identity);
+                GameObject navPoint = GameObject.Find("NavMeshDestinationPoint(Clone)");
+                _movePositionTransform = navPoint.transform;
+            } 
+            else
+            {
+                GameObject navPoint = GameObject.Find("NavMeshDestinationPoint(Clone)");
+                navPoint.transform.position = newPosition;
+                _movePositionTransform = navPoint.transform;
+            }
+
+            yield return new WaitForSeconds(5f);
+        }
     }
 }
