d112708121365584469df05f284a13c6b5d64434
Rafal <rafal.z.malucha@gmail.com>
2026-07-23 00:27:53 +0200

    add procedural test to main menu

--- Assets/_Scripts/Menus/MainMenu.cs
+++ Assets/_Scripts/Menus/MainMenu.cs
@@ -4,11 +4,18 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string _sceneName;
+    [SerializeField] private string _testProceduralSceneName;
     
     public void Play()
     {
         SceneManager.LoadScene(_sceneName);
     }
+
+    public void TestProcedural()
+    {
+        SceneManager.LoadScene(_testProceduralSceneName);
+    }
+
 
     public void Quit()
     {
